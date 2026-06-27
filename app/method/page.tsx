154580ee96d441c4362cd2d99a0e449b11c0298e
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-06-27 14:52:03 +0200

    feat: add method route

---
+++ app/method/page.tsx
@@ -0,0 +1,1 @@
+export default function Page(){return <main className="container page-section"><h1 className="page-title">Method</h1><div className="card content-card"><p>ScanAdmin estimate page.</p></div></main>}
