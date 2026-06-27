2bfa6480546a145404aba94c5a76f65c47c2c0b2
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-06-28 01:15:51 +0200

    chore: type root layout children

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from 'react';
 import { SiteFooter } from '@/components/SiteFooter';
 import { SiteHeader } from '@/components/SiteHeader';
 import './globals.css';
 
-export default function RootLayout({ children }: { children: any }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr">
       <body>
